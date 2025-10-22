e315d1273f0c6215b4e8540b7618499ddfdc095f
Mingas <miguelpinotty@gmail.com>
2025-10-22 14:52:49 -0300

    MERDA

--- src/interface/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Interface Module
-================
-Interface interativa para Jupyter com widgets.
-"""
-
-from .jupyter_interface import YOLOBenchmarkInterface
-
-__all__ = ['YOLOBenchmarkInterface']
